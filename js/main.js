a42e6f4b5cbcc7a4db698280c88f16f8c25f2fc6
Neos Santo <saintogod@gmail.com>
2014-03-01 01:29:18 +0800

    remove some unused file

--- js/main.js
+++ js/main.js
@@ -1,56 +1,87 @@
 require.config({
     baseUrl: 'js',
     paths: {
-        jquery      : 'lib/jquery/jquery-1.10.2',
-        bootstrap   : 'lib/bootstrap/bootstrap',
-        angular     : 'lib/angular/angular',
-        ngRoute     : 'lib/angular/angular-route',
-        ngCookies   : 'lib/angular/angular-cookies',
+        jquery: 'lib/jquery/jquery-2.1.0',
+        bootstrap: 'lib/bootstrap/bootstrap',
+        angular: 'lib/angular/angular',
+        ngRoute: 'lib/angular/angular-route',
+        ngCookies: 'lib/angular/angular-cookies',
         localStorage: 'lib/angular/plugins/angular.LocalStorage',
-        ngGrid      : 'lib/angular/plugins/ng-grid-2.0.7',
-        ngResource  : 'lib/angular/angular-resource',
-        modernizr   : 'lib/modernizr/modernizr.custom',
-        jqueryui    : 'lib/jquery/plugins/jquery-ui/jquery-ui-1.10.3.custom',
+        ngGrid: 'lib/angular/plugins/ng-grid-2.0.7',
+        ngResource: 'lib/angular/angular-resource',
+        modernizr: 'lib/modernizr/modernizr.custom',
+        jqueryui: 'lib/jquery/plugins/jquery-ui/jquery-ui.custom',
         multisortable: 'lib/jquery/plugins/jquery-multisortable/jquery.multisortable',
-        jnotify     : 'lib/jquery/plugins/jquery-notify/jquery.jnotify',
-        jlayout     : 'lib/jquery/plugins/jquery-layout/jquery.layout-latest',
-        jminimalect : 'lib/jquery/plugins/jquery-minimalect/jquery.minimalect',
-        jslimscroll : 'lib/jquery/plugins/jquery-slimscroll/jquery.slimscroll',
-        btswitch    : 'lib/bootstrap/plugins/bootstrap-switch/bootstrap-switch'
+        jnotify: 'lib/jquery/plugins/jquery-notify/jquery.jnotify',
+        jlayout: 'lib/jquery/plugins/jquery-layout/jquery.layout-latest',
+        jminimalect: 'lib/jquery/plugins/jquery-minimalect/jquery.minimalect',
+        jslimscroll: 'lib/jquery/plugins/jquery-slimscroll/jquery.slimscroll',
+        btswitch: 'lib/bootstrap/plugins/bootstrap-switch/bootstrap-switch'
     },
     shim: {
-        "modernizr"     : { exports: "modernizr" },
-        "jquery"        : { exports: '$' },
-        "angular"       : { exports: "angular" },
-        "bootstrap"     : { deps: ["jquery"] },
-        "btswitch"      : { deps: ["jquery", "bootstrap"] },
-        "jqueryui"      : { deps: ["jquery"], exports: "jqueryui" },
-        "multisortable" : { deps: ["jquery", "jqueryui"] },
-        "jnotify"       : { deps: ["jquery"], exports: "jnotify" },
-        "jlayout"       : { deps: ["jquery", "jqueryui"], exports: "jlayout" },
-        "jminimalect"   : { deps: ["jquery"] },
-        "jslimscroll"   : { deps: ["jquery"] },
-        "ngResource"    : { deps: ["angular"] },
-        "ngRoute"       : { deps: ["angular"] },
-        "ngCookies"     : { deps: ["angular"] },
-        "localStorage"  : { deps: ["ngCookies"] },
-        "ngGrid"        : { deps: ["jlayout", "ngResource"]}
+        "modernizr": {
+            exports: "modernizr"
+        },
+        "jquery": {
+            exports: '$'
+        },
+        "angular": {
+            exports: "angular"
+        },
+        "bootstrap": {
+            deps: ["jquery"]
+        },
+        "btswitch": {
+            deps: ["jquery", "bootstrap"]
+        },
+        "jqueryui": {
+            deps: ["jquery"],
+            exports: "jqueryui"
+        },
+        "multisortable": {
+            deps: ["jquery", "jqueryui"]
+        },
+        "jnotify": {
+            deps: ["jquery"],
+            exports: "jnotify"
+        },
+        "jlayout": {
+            deps: ["jquery", "jqueryui"],
+            exports: "jlayout"
+        },
+        "jminimalect": {
+            deps: ["jquery"]
+        },
+        "jslimscroll": {
+            deps: ["jquery"]
+        },
+        "ngResource": {
+            deps: ["angular"]
+        },
+        "ngRoute": {
+            deps: ["angular"]
+        },
+        "ngCookies": {
+            deps: ["angular"]
+        },
+        "localStorage": {
+            deps: ["ngCookies"]
+        },
+        "ngGrid": {
+            deps: ["jlayout", "ngResource"]
+        }
     },
     priority: [
         "jquery",
         "angular"
     ]
 });
-
 window.name = "NG_DEFER_BOOTSTRAP!";
-
-require(['jquery', 'angular', 'app/init', 'modernizr', 'bootstrap'],
-    function($, angular, init, modernizr) {
-        'use strict';
-        var $html = angular.element(document.getElementsByTagName('html')[0]);
-
-        angular.element().ready(function() {
-            $html.addClass('ng-app');
-            angular.bootstrap($html, [init['name']]);
-        });
+require(['jquery', 'angular', 'app/init', 'modernizr', 'bootstrap'], function($, angular, init, modernizr) {
+    'use strict';
+    var $html = angular.element(document.getElementsByTagName('html')[0]);
+    angular.element().ready(function() {
+        $html.addClass('ng-app');
+        angular.bootstrap($html, [init['name']]);
     });
+});
